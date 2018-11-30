508d131d05674cc14c2ac424fe4f2bf26560884c
Camilo Andres <42223946+Camilomf321@users.noreply.github.com>
2018-11-29 19:01:30 -0500

    Update clase_dificultad.py

--- clase_dificultad.py
+++ clase_dificultad.py
@@ -1,17 +1,20 @@
+#clase dificultad -> esta clase muestra las dificultades que se pueden escoger en el juego
 class dificultad():
-
+    #numeroMinas = numero de minas segun la dificultad del juego
+    #nivelDificultad = se despliega en el juego una serie de opciones de dificultad de juego
     numeroMinas = 0
     nivelDificultad = ''
 
+    # iniciar dificultaf
     def __init__(self):
         self
-
+    # ver minas 
     def get_nMinas(self):
         return self.numeroMinas
-
+    # ver dificultad
     def get_nivelDificultad(self):
         return self.nivelDificultad
-
+    # cambiar dificultad 
     def setDificultad(self,dificultad):
         self.nivelDificultad = dificultad;
         if (self.nivelDificultad == 'Facil'):
